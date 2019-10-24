5a5b60fc907b996d485c731f965b32e42168c41e
Shriharsha M <shriharsha05@gmail.com>
2019-10-25 00:08:23 +0530

    Initial setup

---
+++ app.py
@@ -0,0 +1,41 @@
+"""
+Flask-PWA Template
+Developer : Shriharsha M [shriharsha05@computer.org]
+"""
+
+from flask import Flask, render_template, url_for, redirect, request, session, flash
+from gevent.pywsgi import WSGIServer
+
+
+#flask config
+app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+  return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+  return render_template("404.html"), 404
+
+
+@app.route('/sw.js', methods=['GET'])
+def sw():
+    return app.send_static_file('sw.js')
+
+
+@app.route('/manifest.json')
+def manifest():
+    return app.send_static_file('manifest.json')
+
+
+if __name__ == "__main__":
+    # Development
+    # app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
+
+    #Production
+    http_server = WSGIServer(('', 8080), app)
+    http_server.serve_forever()
