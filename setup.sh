04301014f76c0eae638feb6db8f9e84c6ddfe8fe
Shriharsha M <shriharsha05@gmail.com>
2019-11-21 12:44:48 +0530

    Setup commands

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python3.7
+sudo apt install python3-pip
+sudo pip3 install virtualenv
+
+cd ..
+virtualenv venv
+source venv/bin/activate
+cd 'PWA Template'/
+pip3 install -r requirements.txt
